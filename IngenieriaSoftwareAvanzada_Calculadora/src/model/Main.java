7e5f62ef87ae9eb2bc49a5a2cc193df72eb9822c
Jorge Zarza <jorge.zarza057@comunidadunir.net>
2024-05-28 19:14:51 +0200

    Jorge Zarza - Subida Clase Calculadora y Main

---
+++ IngenieriaSoftwareAvanzada_Calculadora/src/model/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+
+        // Ejemplos de uso
+        System.out.println("Suma: " + calc.sumar(2.0, 3.0));
+
+    }
+
+}
